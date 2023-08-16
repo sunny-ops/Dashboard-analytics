b992c962881113b19a4b8645788f30bd588869be
shizhong <shizhong1989@gmail.com>
2023-08-16 11:23:36 -0700

    Add img congrat

--- src/components/Analytics/Analytics.js
+++ src/components/Analytics/Analytics.js
@@ -5,6 +5,7 @@ import { styled } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import { useTheme } from "@mui/material/styles";
+import "./Analytics.css";
 
 const Item = styled(Paper)(({ theme }) => ({
   // backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -21,7 +22,7 @@ function Analytics(props) {
   console.log("main", theme.palette.primary.main);
   return (
     <>
-      <Box className="mt-5 default">
+      <Box sx={{ flexGrow: 1 }} className="mt-5 default-color">
         <Grid container spacing={2}>
           <Grid item xs={8}>
             <Item sx={{ color: "inherit" }}>
@@ -38,7 +39,13 @@ function Analytics(props) {
                   View Badges
                 </Button>
               </div>
-              <div></div>
+              <div>
+                <img
+                  alt="Congratulations John"
+                  src="https://static.vecteezy.com/system/resources/previews/021/449/803/non_2x/cute-girl-with-computer-working-on-laptop-cartoon-icon-illustration-people-technology-icon-concept-art-vector.jpg"
+                  class="congrat-img"
+                />
+              </div>
             </Item>
           </Grid>
           <Grid item xs={2}>
