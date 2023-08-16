9ed9ee517a54d553d739aa2a09c969712a331014
shizhong <shizhong1989@gmail.com>
2023-08-16 15:08:30 -0700

    Before modify bootstrap

--- src/components/Analytics/Analytics.js
+++ src/components/Analytics/Analytics.js
@@ -5,6 +5,8 @@ import { styled } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import { useTheme } from "@mui/material/styles";
+import MoreVertOutlinedIcon from "@mui/icons-material/MoreVertOutlined";
+import ArrowUpwardOutlinedIcon from "@mui/icons-material/ArrowUpwardOutlined";
 import "./Analytics.css";
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -25,16 +27,23 @@ function Analytics(props) {
       <Box sx={{ flexGrow: 1 }} className="mt-5 default-color">
         <Grid container spacing={2}>
           <Grid item xs={8}>
-            <Item sx={{ color: "inherit" }}>
+            <Item
+              sx={{ color: "inherit" }}
+              className="d-flex flex-direction-row justify-content-between box-padding g-5"
+            >
               <div>
-                <h5 style={{ color: themeColor }}>Congratulations John!</h5>
+                <h4 style={{ color: themeColor }} className="mt-2">
+                  Congratulations John!
+                </h4>
                 <p>
-                  You have done 72% more sales today. Check your new badge in
-                  your profile.
+                  You have done 72% more sales today. <br></br>Check your new
+                  badge in your profile.
                 </p>
+
                 <Button
                   variant="outlined"
                   sx={{ color: themeColor, borderColor: themeColor }}
+                  className="mt-3"
                 >
                   View Badges
                 </Button>
@@ -52,7 +61,20 @@ function Analytics(props) {
             <Item>xs=2</Item>
           </Grid>
           <Grid item xs={2}>
-            <Item>xs=2</Item>
+            <Item className="box-padding default-color">
+              <div style={{ height: "160px" }}>
+                <div className="d-flex flex-direction-row mb-3">
+                  <img></img>
+                  <MoreVertOutlinedIcon />
+                </div>
+                <h5 className="mb-2">Sales</h5>
+                <h5 className="default-dark mb-2">$4,679</h5>
+                <div className="d-flex flex-direction-row">
+                  <ArrowUpwardOutlinedIcon />
+                  <p>28.14%</p>
+                </div>
+              </div>
+            </Item>
           </Grid>
         </Grid>
       </Box>
