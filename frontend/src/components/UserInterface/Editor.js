03a4ee4258e420c94a83ef7962cf50f7bebb9ad6
shizhong <shizhong1989@gmail.com>
2023-08-23 11:18:24 -0700

    Add Icons page

--- frontend/src/components/UserInterface/Editor.js
+++ frontend/src/components/UserInterface/Editor.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Editor(props) {
-  return <div>Editor</div>;
+  return (
+    <div style={{}} className="default round-border mt-3 p-5">
+      <div>APP</div>
+      <div className="font-lg" style={{ color: "black" }}>
+        Editor
+      </div>
+    </div>
+  );
 }
 
 export default Editor;
