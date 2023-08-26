f9353bf73955062719cec3c7abe6556538d8e5e0
shizhong <shizhong1989@gmail.com>
2023-08-25 21:08:06 -0700

    Add components.js

--- frontend/src/components/UserInterface/Components.js
+++ frontend/src/components/UserInterface/Components.js
@@ -3,27 +3,414 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import InboxIcon from "@mui/icons-material/Inbox";
+import DraftsIcon from "@mui/icons-material/Drafts";
+import Rating from "@mui/material/Rating";
+import Badge from "@mui/material/Badge";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import MailIcon from "@mui/icons-material/Mail";
+import Switch from "@mui/material/Switch";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import AppBar from "@mui/material/AppBar";
+
+import Toolbar from "@mui/material/Toolbar";
+
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import FormGroup from "@mui/material/FormGroup";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: "center",
+  //   textAlign: "center",
   color: theme.palette.text.secondary,
 }));
 
 function Components(props) {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  const [value, setValue] = React.useState(2);
+
+  const [count, setCount] = React.useState(1);
+  const [invisible, setInvisible] = React.useState(false);
+
+  const handleBadgeVisibility = () => {
+    setInvisible(!invisible);
+  };
+
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleChange1 = (event) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }} className="mt-4 default-color">
       <Grid container spacing={2}>
         <Grid item className="col-lg-6 col-12">
-          <Item sx={{ color: "inherit" }} className="round-border">
-            <div>Accordion</div>
+          <Item sx={{ color: "inherit" }} className="round-border box-padding">
+            <div className="font-lg default-dark mb-3">Accordion</div>
+            <div className="font-sm default-color mb-3">
+              Use Accordion, AccordionSummary and AccordionDetails components to
+              make a simple accordion.
+            </div>
+            <div className="default-color">
+              <Accordion
+                expanded={expanded === "panel1"}
+                onChange={handleChange("panel1")}
+                style={{ color: "inherit" }}
+              >
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1bh-content"
+                  id="panel1bh-header"
+                >
+                  <Typography sx={{ width: "33%", flexShrink: 0 }}>
+                    General settings
+                  </Typography>
+                  <Typography sx={{ color: "text.secondary" }}>
+                    I am an accordion
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    Nulla facilisi. Phasellus sollicitudin nulla et quam mattis
+                    feugiat. Aliquam eget maximus est, id dignissim quam.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion
+                expanded={expanded === "panel2"}
+                onChange={handleChange("panel2")}
+                style={{ color: "inherit" }}
+              >
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel2bh-content"
+                  id="panel2bh-header"
+                >
+                  <Typography sx={{ width: "33%", flexShrink: 0 }}>
+                    Users
+                  </Typography>
+                  <Typography sx={{ color: "text.secondary" }}>
+                    You are currently not an owner
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    Donec placerat, lectus sed mattis semper, neque lectus
+                    feugiat lectus, varius pulvinar diam eros in elit.
+                    Pellentesque convallis laoreet laoreet.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion
+                expanded={expanded === "panel3"}
+                onChange={handleChange("panel3")}
+                style={{ color: "inherit" }}
+              >
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel3bh-content"
+                  id="panel3bh-header"
+                >
+                  <Typography sx={{ width: "33%", flexShrink: 0 }}>
+                    Advanced settings
+                  </Typography>
+                  <Typography sx={{ color: "text.secondary" }}>
+                    Filtering has been entirely disabled for whole web server
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    Nunc vitae orci ultricies, auctor nunc in, volutpat nisl.
+                    Integer sit amet egestas eros, vitae egestas augue. Duis vel
+                    est augue.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion
+                expanded={expanded === "panel4"}
+                onChange={handleChange("panel4")}
+                style={{ color: "inherit" }}
+              >
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel4bh-content"
+                  id="panel4bh-header"
+                >
+                  <Typography sx={{ width: "33%", flexShrink: 0 }}>
+                    Personal data
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    Nunc vitae orci ultricies, auctor nunc in, volutpat nisl.
+                    Integer sit amet egestas eros, vitae egestas augue. Duis vel
+                    est augue.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+            </div>
           </Item>
         </Grid>
         <Grid item className="col-lg-6 col-12">
-          <Item className="round-border" sx={{ color: "inherit" }}>
-            haha
+          <Item className="round-border box-padding" sx={{ color: "inherit" }}>
+            <div className="font-lg default-dark mb-3">Alert</div>
+            <div className="font-sm default-color mb-3">
+              Use severity={"error" | "warning" | "info" | "success"} prop with
+              Alert component for different colored alerts.
+            </div>
+            <Stack sx={{ width: "100%" }} spacing={2}>
+              <Alert severity="error">
+                This is an error alert — check it out!
+              </Alert>
+              <Alert severity="warning">
+                This is a warning alert — check it out!
+              </Alert>
+              <Alert severity="info">
+                This is an info alert — check it out!
+              </Alert>
+              <Alert severity="success">
+                This is a success alert — check it out!
+              </Alert>
+            </Stack>
+          </Item>
+        </Grid>
+        <Grid item className="col-lg-6 col-12">
+          <Item sx={{ color: "inherit" }} className="round-border box-padding">
+            <div className="font-lg default-dark mb-3">Simple List</div>
+            <div className="font-sm default-color mb-3">
+              Lists are a continuous group of text or images.
+            </div>
+            <Box
+              sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+            >
+              <nav aria-label="main mailbox folders">
+                <List>
+                  <ListItem disablePadding>
+                    <ListItemButton>
+                      <ListItemIcon>
+                        <InboxIcon />
+                      </ListItemIcon>
+                      <ListItemText primary="Inbox" />
+                    </ListItemButton>
+                  </ListItem>
+                  <ListItem disablePadding>
+                    <ListItemButton>
+                      <ListItemIcon>
+                        <DraftsIcon />
+                      </ListItemIcon>
+                      <ListItemText primary="Drafts" />
+                    </ListItemButton>
+                  </ListItem>
+                </List>
+              </nav>
+              <Divider />
+              <nav aria-label="secondary mailbox folders">
+                <List>
+                  <ListItem disablePadding>
+                    <ListItemButton>
+                      <ListItemText primary="Trash" />
+                    </ListItemButton>
+                  </ListItem>
+                  <ListItem disablePadding>
+                    <ListItemButton>
+                      <ListItemText primary="Spam" />
+                    </ListItemButton>
+                  </ListItem>
+                </List>
+              </nav>
+            </Box>
+          </Item>
+        </Grid>
+        <Grid item className="col-lg-6 col-12">
+          <Item className="round-border box-padding" sx={{ color: "inherit" }}>
+            <div className="font-lg default-dark mb-3">Ratings</div>
+            <div className="font-sm default-color mb-3">
+              Use name prop to name the rating and use value or defaultValue
+              prop to set any initial value to a rating.
+            </div>
+            <Box className="d-flex flex-column mb-2">
+              <Typography component="legend">Controlled</Typography>
+              <Rating
+                name="simple-controlled"
+                value={value}
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                }}
+              />
+              <Typography component="legend">Read only</Typography>
+              <Rating name="read-only" value={value} readOnly />
+              <Typography component="legend">Disabled</Typography>
+              <Rating name="disabled" value={value} disabled />
+              <Typography component="legend">No rating given</Typography>
+              <Rating name="no-value" value={null} />
+            </Box>
+          </Item>
+        </Grid>
+        <Grid item className="col-lg-6 col-12">
+          <Item sx={{ color: "inherit" }} className="round-border box-padding">
+            <div className="font-lg default-dark mb-3">Badge</div>
+            <div className="font-sm default-color mb-3">
+              Badge generates a small badge to the top-right of its child(ren)
+            </div>
+            <Box
+              sx={{
+                color: "action.active",
+                display: "flex",
+                flexDirection: "column",
+                "& > *": {
+                  marginBottom: 2,
+                },
+                "& .MuiBadge-root": {
+                  marginRight: 4,
+                },
+              }}
+            >
+              <div>
+                <Badge color="secondary" badgeContent={count}>
+                  <MailIcon />
+                </Badge>
+                <ButtonGroup>
+                  <Button
+                    aria-label="reduce"
+                    onClick={() => {
+                      setCount(Math.max(count - 1, 0));
+                    }}
+                  >
+                    <RemoveIcon fontSize="small" />
+                  </Button>
+                  <Button
+                    aria-label="increase"
+                    onClick={() => {
+                      setCount(count + 1);
+                    }}
+                  >
+                    <AddIcon fontSize="small" />
+                  </Button>
+                </ButtonGroup>
+              </div>
+              <div>
+                <Badge color="secondary" variant="dot" invisible={invisible}>
+                  <MailIcon />
+                </Badge>
+                <FormControlLabel
+                  sx={{ color: "text.primary" }}
+                  control={
+                    <Switch
+                      checked={!invisible}
+                      onChange={handleBadgeVisibility}
+                    />
+                  }
+                  label="Show Badge"
+                />
+              </div>
+            </Box>
+          </Item>
+        </Grid>
+        <Grid item className="col-lg-6 col-12">
+          <Item sx={{ color: "inherit" }} className="round-border box-padding">
+            <div className="font-lg default-dark mb-3">App Bar</div>
+            <div className="font-sm default-color mb-3">
+              The App Bar displays information and actions relating to the
+              current screen.
+            </div>
+            <Box sx={{ flexGrow: 1 }}>
+              <FormGroup>
+                <FormControlLabel
+                  control={
+                    <Switch
+                      checked={auth}
+                      onChange={handleChange1}
+                      aria-label="login switch"
+                    />
+                  }
+                  label={auth ? "Logout" : "Login"}
+                />
+              </FormGroup>
+              <AppBar position="static">
+                <Toolbar>
+                  <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 2 }}
+                  >
+                    <MenuIcon />
+                  </IconButton>
+                  <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    Photos
+                  </Typography>
+                  {auth && (
+                    <div>
+                      <IconButton
+                        size="large"
+                        aria-label="account of current user"
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        onClick={handleMenu}
+                        color="inherit"
+                      >
+                        <AccountCircle />
+                      </IconButton>
+                      <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorEl}
+                        anchorOrigin={{
+                          vertical: "top",
+                          horizontal: "right",
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                          vertical: "top",
+                          horizontal: "right",
+                        }}
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                      >
+                        <MenuItem onClick={handleClose}>Profile</MenuItem>
+                        <MenuItem onClick={handleClose}>My account</MenuItem>
+                      </Menu>
+                    </div>
+                  )}
+                </Toolbar>
+              </AppBar>
+            </Box>
           </Item>
         </Grid>
       </Grid>
